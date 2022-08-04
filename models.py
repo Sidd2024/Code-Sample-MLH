1ae75a785c693b6f55f9a8bff88520efd384b80e
Siddhant Sharma <86948743+Sidd2024@users.noreply.github.com>
2022-08-04 12:33:27 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,3 @@
-from email.policy import default
-from random import choices
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from multiselectfield import MultiSelectField
